efedccf968344bd887cce4def1c29fb7bc7a071b
Kieran Jaggernauth <thejugg63@gmail.com>
2022-08-15 19:51:59 -0500

    added keypress event

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 'use strict'
 
+function buttonAnimation(key){
+
+}
+
+async function playSound(soudName){
+    
+    var audio = new Audio("./sounds/"+soudName+".mp3");
+
+    audio.play();
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
+
     var buttons = document.getElementsByTagName('button');
 
     for (let i = 0; i < buttons.length; i++) {
-        const button = buttons[i];
-        button.addEventListener('click',()=>{
-            console.log(button.id);
-            var audio = new Audio("./sounds/"+button.id+".mp3");
-            audio.play();
+
+        let button = buttons[i];
+
+        button.addEventListener('click',function(event){
+
+                button.style.color = "white";
+
+                playSound(button.id);
+
+                buttonAnimation(button.id)
+
         });
+
     }
+})
+
+window.addEventListener('keydown', function(event){
+
+    playSound(event.code.slice(-1).toLowerCase());
+
+    buttonAnimation(event.code.slice(-1).toLowerCase());
+
 })
